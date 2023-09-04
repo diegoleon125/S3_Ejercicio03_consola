3210ed07168cb8941f460bcafb48d2980f129934
user <user@LAPTOP-2DK3J1EI>
2023-09-04 18:20:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Inserte edad: ");
+int edad = int.Parse(Console.ReadLine());
+Console.Write("Inserte adivinanzas acertadas: ");
+int adiv = int.Parse(Console.ReadLine());
+float mont=0f;
+if (edad < 18) {
+    if (adiv < 3)
+    {
+        mont = 50.00f;
+    }
+    else
+    {
+        mont = 80.00f;
+    }
+} else {
+    if (adiv < 3)
+    {
+        mont = 30.00f;
+    }
+    else
+    {
+        mont = 50.00f;
+    }
+}
+mont += 5*edad + 2*adiv;
+Console.WriteLine("el premio es: " + mont);
